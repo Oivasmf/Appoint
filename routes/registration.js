350fda1ec9eb35f00667c1245f1e66807acce91e
Fernando Jacques Peiretti <61033888+fernandopeiretti@users.noreply.github.com>
2021-11-23 22:50:36 -0300

    server.js, .env, registration.js
    Desenvolvimento do registration.js e ajustes nos outros arquivos

--- routes/registration.js
+++ routes/registration.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 router.post('/', (req, res) => {
     /*recebe do front: 
-    {"nome_paciente":"aaaa", "email_paciente":"bbbb", "senha_paciente":"cccc", "confirmacao_senha":c2c2", "cpf_paciente":"dddd", "data_nascimento_paciente":"yyyy-mm-dd", "sexo":"x", "telefone"(opcional):"eeee","nome_rua":"ffff", "numero":"gggg", "complemento":"hhhh", //DEFINIR BAIRRO}
+    {"nome_paciente":"aaaa", "email_paciente":"bbbb", "senha_paciente":"cccc", "confirmacao_senha":c2c2", "cpf_paciente":"dddd", "data_nascimento_paciente":"yyyy-mm-dd", "sexo":"x", "telefone"(opcional):"eeee","nome_rua":"ffff", "numero":"gggg", "complemento":"hhhh"}
 
     Se o cpf constar no banco de dados, impede o registro. Senão, insere um registro na tabela Pacientes.*/
     knex('pacientes')
@@ -14,17 +14,26 @@ router.post('/', (req, res) => {
     })
     .select('*')
     .then(data => {
-        res.status(401).json({ //caso de cpf já cadastrado
-            err:'Um usuário com este CPF já consta no sistema.'
-        });
+        if(data[0]){ //caso de cpf já cadastrado
+            res.status(401).json({
+                err:'Um usuário com este CPF já consta no sistema.'
+            });
+        } else{ //cadastro de usuário
+            console.log(req.body.senha_paciente);
+            var encodedPassword = encode().value(req.body.senha_paciente); //aplica o hashcode para a senha
+                 knex('pacientes') //TESTAR
+                .insert({'nome_paciente':req.body.nome_paciente, 'email_paciente':req.body.email_paciente, 'senha_paciente':encodedPassword, 'cpf_paciente':req.body.cpf_paciente, 'data_nascimento_paciente':req.body.data_nascimento_paciente, 'sexo':req.body.sexo, 'telefone':req.body.telefone,'id_cidade':req.body.id_cidade, 'bairro':req.body.bairro, 'nome_rua':req.body.nome_rua, 'numero':req.body.numero, 'complemento':req.body.complemento,})
+                .then(resposta =>{
+                    console.log(resposta);
+                })
+        }
     })
     .catch(() => {
-        if(req.body.senha_paciente != req.body.confimacao_senha){
-            req.status(401).json({
+        if(req.body.senha_paciente != req.body.confirmacao_senha){
+            res.status(401).json({
                 err:'Usuário não pôde ser cadastrado: senhas divergentes.'
             });
         } else{
-            var encodedPassword = encode().value(req.body.senha_paciente); //aplica o hashcode para a senha
             /*if(req.body.telefone){
                 knex('pacientes') //TESTAR
                 .insert({"nome_paciente":"req.body.nome_paciente", "email_paciente":"req.body.email_paciente", "senha_paciente":encodedPassword, "cpf_paciente":"req.body.cpf_paciente", "data_nascimento_paciente":"req.body.data_nascimento_paciente", "sexo":"req.body.sexo", "telefone"(opcional):"req.body.telefone","nome_rua":"req.body.nome_rua", "numero":"req.body.numero", "complemento":"req.body.complemento", //DEFINIR BAIRRO})
--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const cors = require("cors");
 const authentication = require("./auth");
 
 const loginRouter = require('./routes/login');
+const registrationRouter = require('./routes/registration');
 /*const passwordRequestRouter = require('./routes/passwordRequest');
 const passwordChangeRouter = require('./routes/passwordChange');
-const registrationRouter = require('./routes/registration');
 const appointmentListRouter = require('./routes/appointmentList');
 const makeAppointmentRouter = require('./routes/makeAppointment');*/
 
@@ -23,9 +23,9 @@ app.use(cors());
 
 app.use(express.json());
 app.use('/login', loginRouter);
+app.use('/registration', registrationRouter);
 /*app.use('/passwordRequest', passwordRequestRouter);
 app.use('/passwordChange', passwordChangeRouter);
-app.use('/registration', registrationRouter);
 app.use('/appointmentList', authentication, appointmentListRouter);
 app.use('/makeAppointment', authentication, makeAppointmentRouter);*/
 
